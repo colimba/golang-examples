636fc046ea19e6e969de2ce6e6fcc402f5b8a00a
andresG <andres.giacosa@tech-house.tech>
2025-02-03 08:58:14 -0300

    agrega stores al ejemplo con cobra

--- 02-refactor-to-cobra/cmd/beers-cli/main.go
+++ 02-refactor-to-cobra/cmd/beers-cli/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/CodelyTV/golang-examples/02-refactor-to-cobra/internal/cli"
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	rootCmd := &cobra.Command{Use: "beers-cli"}
-	rootCmd.AddCommand(cli.InitBeersCmd())
-	rootCmd.Execute()
+	beerCmd := &cobra.Command{Use: "beers-cli"}
+	storeCmd := &cobra.Command{Use: "stores-cli"}
+
+	beerCmd.AddCommand(cli.InitBeersCmd())
+	storeCmd.AddCommand(cli.InitStoresCmd())
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "beers":
+			beerCmd.SetArgs(os.Args[1:])
+			beerCmd.Execute()
+		case "stores":
+			storeCmd.SetArgs(os.Args[1:])
+			storeCmd.Execute()
+		default:
+			fmt.Println("Invalid command")
+		}
+
+	}
+
 }
